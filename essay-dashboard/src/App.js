cdc3a9d3e1b0bfe0b3e49fd70e61ece047d14cf6
Rishi1204 <44839841+Rishi1204@users.noreply.github.com>
2022-02-06 18:24:29 -0500

    input/output front-end
    created input/output boxes that can take in text and store it.

--- essay-dashboard/src/App.js
+++ essay-dashboard/src/App.js
@@ -1,11 +1,43 @@
-import "./App.css";
+import './App.css';
+import arrow from './arrow.png';
+import React, { useState, setState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">Essay Dashboard</header>
-    </div>
-  );
+	const [input, setInput] = useState('');
+	function handleChange(event) {
+		setInput(event.target.value);
+	}
+	function handleSubmit(event) {
+		alert(input);
+		event.preventDefault();
+	}
+	return (
+		<div className="App">
+			<div className="text-box">
+				<div className="text-input-output">
+					<form>
+						<textarea
+							type="text"
+							className="input"
+							placeholder="Type something..."
+							onChange={handleChange}
+							align="left"
+						></textarea>
+					</form>
+					<button onClick={handleSubmit}>
+						<img src={arrow} className="arrow" alt="arrow"></img>
+					</button>
+					<form>
+						<textarea
+							type="text"
+							className="output"
+							placeholder="Response..."
+						></textarea>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
