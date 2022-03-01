a0145e358ff14c43f03d9085ab28c836d98119e3
Rishi1204 <44839841+Rishi1204@users.noreply.github.com>
2022-02-28 22:47:31 -0500

    merged rishi with sean

--- essay-dashboard/src/routes/home.js
+++ essay-dashboard/src/routes/home.js
@@ -4,6 +4,24 @@ import React, { useState, setState, useRef, useEffect } from 'react';
 
 export default function Home() {
     const [input, setInput] = useState('');
+	const [styles, setStyles] = useState([
+		{
+			function_btn: 'function-btn',
+			description: 'description',
+			heading: 'heading',
+		},
+		{
+			function_btn: 'function-btn',
+			description: 'description',
+			heading: 'heading',
+		},
+		{
+			function_btn: 'function-btn',
+			description: 'description',
+			heading: 'heading',
+		},
+	]);
+
 	function handleChange(event) {
 		setInput(event.target.value);
 	}
@@ -11,48 +29,124 @@ export default function Home() {
 		alert(input);
 		event.preventDefault();
 	}
-    return (
-        <div className="box">
-			<div className="text-input-output">
-					<form>
-						<textarea
-							type="text"
-							className="input"
-							placeholder="Type something..."
-							onChange={handleChange}
-							align="left"
-						></textarea>
-					</form>
-					<button onClick={handleSubmit}>
-						<img src={arrow} className="arrow" alt="arrow"></img>
-					</button>
-					<form>
-						<textarea
-							type="text"
-							className="output"
-							placeholder="Response..."
-						></textarea>
-					</form>
-				
+	var addStyles = (index) => {
+		let newStyles = [...styles];
+		for (let i = 0; i < newStyles.length; i++) {
+			if (i == index) {
+				newStyles[i] = {
+					function_btn: 'active-function-btn',
+					description: 'active-description',
+					heading: 'active-heading',
+				};
+			} else {
+				newStyles[i] = {
+					function_btn: 'function-btn',
+					description: 'description',
+					heading: 'heading',
+				};
+			}
+		}
+		setStyles(newStyles);
+		console.log(newStyles);
+	};
+	useEffect(() => {
+		console.log(styles);
+	}, [styles]);
+
+	function SidebarButton(props) {
+		return (
+			<button
+				onClick={() => addStyles(props.index)}
+				className={styles[props.index].function_btn}
+			>
+				<div className={styles[props.index].heading}>{props.heading}</div>
+				<div className={styles[props.index].description}>
+					{props.description}
 				</div>
+			</button>
+		);
+	}
+
+	function SummarizeInterface(props) {
+		return (
+			<div className="text-input-output">
+				<form>
+					<textarea
+						type="text"
+						className="input"
+						placeholder="Type something..."
+						onChange={handleChange}
+						align="left"
+					></textarea>
+				</form>
+				<button onClick={handleSubmit}>
+					<img src={arrow} className="arrow" alt="arrow"></img>
+				</button>
+				<form>
+					<textarea
+						type="text"
+						className="output"
+						placeholder="Response..."
+					></textarea>
+				</form>
+			</div>
+		);
+	}
+	function SummarizeSentences(props) {
+		return (
+			<div className="text-input-output">
+				<form>
+					<textarea
+						type="text"
+						className="input"
+						placeholder="Type something..."
+						onChange={handleChange}
+						align="left"
+					></textarea>
+				</form>
+				<button onClick={handleSubmit}>
+					<img src={arrow} className="arrow" alt="arrow"></img>
+				</button>
+				<form>
+					<textarea
+						type="text"
+						className="output"
+						placeholder="Response..."
+					></textarea>
+				</form>
+			</div>
+		);
+	}
+	return (
+		<div className="App">
+			<div className="essay-header">
+				<div className="title">Essay Dashboard</div>
+			</div>
+			<div className="box">
+				<SummarizeInterface />
 				<div className="function-sidebar">
 					<div className="btn-holder">
-					<button className="function-btn">
-						<div className="heading">Rephrase sentences</div>
-						<div className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper lobortis justo id laoreet. </div>
-					</button>
-					<button className="function-btn">
-						<div className="heading">Summarize sentences</div>
-						<div className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper lobortis justo id laoreet. </div>
-					</button>
-					<button className="function-btn">
-						<div className="heading">
-							<Link to="/completion">Sentence Completion</Link>
-						</div>
-						<div className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper lobortis justo id laoreet. </div>
-					</button>
+						<SidebarButton
+							index={0}
+							heading="Rephrase sentences"
+							description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+								semper lobortis justo id laoreet."
+						/>
+						<SidebarButton
+							index={1}
+							heading="Summarize sentences"
+							description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+								semper lobortis justo id laoreet."
+						/>
+						<SidebarButton
+							index={2}
+							heading="Create an outline"
+							description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+								semper lobortis justo id laoreet."
+						/>
 					</div>
 				</div>
 			</div>
-    );    
+		</div>
+	);    
 }
