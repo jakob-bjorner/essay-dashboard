c7cea1b34be228d0fd8bac165be3e8f81cfe50de
Rishi1204 <44839841+Rishi1204@users.noreply.github.com>
2022-02-28 22:30:59 -0500

    add. front-end changes

--- essay-dashboard/src/App.js
+++ essay-dashboard/src/App.js
@@ -52,65 +52,98 @@ function App() {
 	useEffect(() => {
 		console.log(styles);
 	}, [styles]);
+
+	function SidebarButton(props) {
+		return (
+			<button
+				onClick={() => addStyles(props.index)}
+				className={styles[props.index].function_btn}
+			>
+				<div className={styles[props.index].heading}>{props.heading}</div>
+				<div className={styles[props.index].description}>
+					{props.description}
+				</div>
+			</button>
+		);
+	}
+
+	function SummarizeInterface(props) {
+		return (
+			<div className="text-input-output">
+				<form>
+					<textarea
+						type="text"
+						className="input"
+						placeholder="Type something..."
+						onChange={handleChange}
+						align="left"
+					></textarea>
+				</form>
+				<button onClick={handleSubmit}>
+					<img src={arrow} className="arrow" alt="arrow"></img>
+				</button>
+				<form>
+					<textarea
+						type="text"
+						className="output"
+						placeholder="Response..."
+					></textarea>
+				</form>
+			</div>
+		);
+	}
+	function SummarizeSentences(props) {
+		return (
+			<div className="text-input-output">
+				<form>
+					<textarea
+						type="text"
+						className="input"
+						placeholder="Type something..."
+						onChange={handleChange}
+						align="left"
+					></textarea>
+				</form>
+				<button onClick={handleSubmit}>
+					<img src={arrow} className="arrow" alt="arrow"></img>
+				</button>
+				<form>
+					<textarea
+						type="text"
+						className="output"
+						placeholder="Response..."
+					></textarea>
+				</form>
+			</div>
+		);
+	}
 	return (
 		<div className="App">
 			<div className="essay-header">
 				<div className="title">Essay Dashboard</div>
 			</div>
 			<div className="box">
-				<div className="text-input-output">
-					<form>
-						<textarea
-							type="text"
-							className="input"
-							placeholder="Type something..."
-							onChange={handleChange}
-							align="left"
-						></textarea>
-					</form>
-					<button onClick={handleSubmit}>
-						<img src={arrow} className="arrow" alt="arrow"></img>
-					</button>
-					<form>
-						<textarea
-							type="text"
-							className="output"
-							placeholder="Response..."
-						></textarea>
-					</form>
-				</div>
+				<SummarizeInterface />
 				<div className="function-sidebar">
 					<div className="btn-holder">
-						<button
-							onClick={() => addStyles(0)}
-							className={styles[0].function_btn}
-						>
-							<div className={styles[0].heading}>Rephrase sentences</div>
-							<div className={styles[0].description}>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-								semper lobortis justo id laoreet.
-							</div>
-						</button>
-						<button
-							onClick={() => addStyles(1)}
-							className={styles[1].function_btn}
-						>
-							<div className={styles[1].heading}>Summarize sentences</div>
-							<div className={styles[1].description}>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-								semper lobortis justo id laoreet.
-							</div>
-						</button>
-						<button
-							onClick={() => addStyles(2)}
-							className={styles[2].function_btn}
-						>
-							<div className={styles[2].heading}>Create an outline</div>
-							<div className={styles[2].description}>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-								semper lobortis justo id laoreet.
-							</div>
-						</button>
+						<SidebarButton
+							index={0}
+							heading="Rephrase sentences"
+							description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+								semper lobortis justo id laoreet."
+						/>
+						<SidebarButton
+							index={1}
+							heading="Summarize sentences"
+							description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+								semper lobortis justo id laoreet."
+						/>
+						<SidebarButton
+							index={2}
+							heading="Create an outline"
+							description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+								semper lobortis justo id laoreet."
+						/>
 					</div>
 				</div>
 			</div>
