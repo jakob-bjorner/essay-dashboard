201f00d681b73b45ac5ef8e81bca3bd31be2b4f7
Ian Wood <iwood33@gatech.edu>
2022-03-01 22:57:50 -0500

    created service function to get rephrase data

--- api/main.py
+++ api/main.py
@@ -2,8 +2,10 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_restful import Api, Resource
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
