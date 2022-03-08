06e78d38a465fd7bd928bb4e79300754c3d58025
Ian Wood <iwood33@gatech.edu>
2022-03-08 18:48:04 -0500

    create test function

--- api/main.py
+++ api/main.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_restful import Api, Resource
 from flask_cors import CORS
+import test_openai
 
 app = Flask(__name__)
 CORS(app)
@@ -79,6 +80,15 @@ class RephraseRequestResource(Resource):
 		db.session.commit()
 		return '', 204
 
+class NewRephraseRequest(Resource):
+	def post(self):
+		message = request.json['message']
+	
+		result = gpt3Rephrase(message)
+		print(result)
+		return result
+
+api.add_resource(AcceptedRephraseRequestListResource, '/rephrase')
 api.add_resource(AcceptedRephraseRequestListResource, '/rephrase-requests/accepted')
 api.add_resource(RephraseRequestListResource, '/rephrase-requests')
 api.add_resource(RephraseRequestResource, '/rephrase-requests/<int:request_id>')
