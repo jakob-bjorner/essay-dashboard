964a579a8777507505f28ecb894467e204c4434a
jakob-bjorner <jbjorner3@gatech.edu>
2022-03-21 01:24:21 -0500

    minor spacing change

--- essay-dashboard/src/services/RephraseRequestService.js
+++ essay-dashboard/src/services/RephraseRequestService.js
@@ -11,14 +11,14 @@ export const getRephraseRequests = async () => {
 
 export const rephraseSentence = async (message) => {
   return fetch("http://localhost:5000/rephrase", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      message,
-    }),
-  })
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message,
+      }),
+    })
     .then((response) => {
       return response.json();
     })
