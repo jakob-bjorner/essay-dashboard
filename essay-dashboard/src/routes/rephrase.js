dc8765b79a268d585311f255970dea21635ed04b
Cal3123 <tettehcaleb@gmail.com>
2022-03-23 15:37:49 -0400

    commit changes

--- essay-dashboard/src/routes/rephrase.js
+++ essay-dashboard/src/routes/rephrase.js
@@ -1,82 +1,84 @@
 import arrow from '../arrow.png';
 import React, { useState, setState, useRef, useEffect } from 'react';
- 
-import {getRephraseRequests} from '../services/RephraseRequestService';
+
+import { rephraseSentence } from '../services/RephraseRequestService';
 
 export default function Rephrase() {
 	const [output, showResult] = useState('');
-  	const [input, setInput] = useState('');
+	const [input, setInput] = useState('');
 	const inputRef = useRef(null);
-  	function handleChange(event) {
-    	setInput(event.target.value);
-  	}
-	function handleSubmit(event) {
-    	alert(input);
-		console.log('hi')
-		getRephraseRequests().then((data) => {
-			console.log(data)
-		})
-		event.preventDefault();
-  	}
-	useEffect(() => {
-		console.log(input);
-	}, [input]);  
-
 	function handleChange(event) {
-    	setInput(event.target.value);
-  	}
+		setInput(event.target.value);
+	}
+	//function handleSubmit(event) {
+	//	alert(input);
+	//	console.log('hi')
+	//	getRephraseRequests().then((data) => {
+	//		console.log(data)
+	//	})
+	//	event.preventDefault();
+	//}
+
 	async function handleSubmit(event) {
-   // setOutput(input);
-    //console.log(input)
-		alert(input);
-		event.preventDefault();
-		 
-		const data = await getRephraseRequests();
+		// setOutput(input);
+		//console.log(input)
+		//alert(input);
+		console.log('hi')
+
+		//getRephraseRequests().then((data) => {
+		//	console.log(data)
+		//})
+		const data = await rephraseSentence(input);
 		showResult(data);
 		console.log(data)
+		event.preventDefault();
 	}
 
-	function IOInterface(props) {
-		return (
-			<div className="IObox">
+	useEffect(() => {
+		console.log(input);
+	}, [input]);
+
+	return (
+
+		<div className="IObox">
+			<form>
+				<textarea
+					ref={inputRef}
+					key="user_name_key"
+					type="text"
+					className="input"
+					placeholder="Type something..."
+
+					onChange={handleChange}
+					value={input}
+					align="left"
+				></textarea>
+			</form>
+			<button onClick={handleSubmit}>
+				<img src={arrow} className="arrow" alt="arrow"></img>
+			</button>
+			<div className="text-rephrase-out">
 				<form>
 					<textarea
 						type="text"
-						className="input"
-						placeholder="Type something..."
-						value = {input}
-						onChange={handleChange}
-						align="left"
+						className="output"
+						placeholder="Response..."
+						value={output}
+						onChange={(e) => showResult(e.target.value)}
+					></textarea>
+					<textarea
+						type="text"
+						className="output"
+						placeholder="Response..."
+					></textarea>
+					<textarea
+						type="text"
+						className="output"
+						placeholder="Response..."
 					></textarea>
 				</form>
-				<button onClick={handleSubmit}>
-					<img src={arrow} className="arrow" alt="arrow"></img>
-				</button>
-        <div className="text-rephrase-out">
-          <form>
-					  <textarea
-						  type="text"
-						  className="output"
-						  placeholder="Response..."
-						  value = {output}
-						  onChange={(e) => showResult(e.target.value)}
-					  ></textarea>
-            <textarea
-						  type="text"
-						  className="output"
-						  placeholder="Response..."
-					  ></textarea>
-            <textarea
-						  type="text"
-						  className="output"
-						  placeholder="Response..."
-					  ></textarea>
-				  </form>
-        </div>
 			</div>
-		);
-	}
-	return (
-		<IOInterface />
-	);      
+		</div>
+
+	);
 }
