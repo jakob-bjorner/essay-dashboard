0a8215b19fec26742c240f003c2d7f3c4306dd18
Ian Wood <iwood33@gatech.edu>
2022-04-12 19:01:29 -0400

    add service function

--- essay-dashboard/src/services/RephraseRequestService.js
+++ essay-dashboard/src/services/RephraseRequestService.js
@@ -1,5 +1,26 @@
-export const getRephraseRequests = async () => {
+export const getRephraseLogs = async () => {
   return fetch("https://essai-api.herokuapp.com/rephrase-logs")
+    .then((response) => {
+      return response.json();
+    })
+    .catch((error) => {
+      console.log(error);
+      return [];
+    });
+};
+
+export const postRephraseLogs = async (original, rephrased, accepted) => {
+  return fetch("https://essai-api.herokuapp.com/rephrase-logs", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      original,
+      rephrased,
+      accepted,
+    }),
+  })
     .then((response) => {
       return response.json();
     })
