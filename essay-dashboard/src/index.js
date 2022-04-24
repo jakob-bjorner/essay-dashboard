6b927ec4fd3bf08eb159a99e4d30c39cb95f9f7c
Rishi1204 <44839841+Rishi1204@users.noreply.github.com>
2022-04-24 01:51:50 -0400

    dark mode implementation

--- essay-dashboard/src/index.js
+++ essay-dashboard/src/index.js
@@ -4,22 +4,33 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState, useRef, useEffect } from 'react';
 import Outline from './routes/outline';
 import Complete from './routes/complete';
 import Rephrase from './routes/rephrase';
+import { ModeContext } from './ModeContext';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="rephrase" element={<Rephrase />} />
-        <Route path="complete" element={<Complete />} />
-        <Route path="outline" element={<Outline />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+function ParentApp() {
+	const [light, setMode] = useState(true);
+	useEffect(() => {
+		console.log('Parent app', light);
+	}, [light]);
+	return (
+		<BrowserRouter>
+			<ModeContext.Provider value={{ light, setMode }}>
+				<Routes>
+					<Route path="/" element={<App />}>
+						<Route path="rephrase" element={<Rephrase />} />
+						<Route path="complete" element={<Complete />} />
+						<Route path="outline" element={<Outline />} />
+					</Route>
+				</Routes>
+			</ModeContext.Provider>
+		</BrowserRouter>
+	);
+}
+
+ReactDOM.render(<ParentApp />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- essay-dashboard/src/routes/rephrase.js
+++ essay-dashboard/src/routes/rephrase.js
@@ -2,38 +2,97 @@ import arrow from '../arrow.svg';
 import copy_icon from '../copy-icon.png';
 import like_btn from '../like-btn.png';
 import dislike_btn from '../dislike-btn.png';
-import React, { useState, setState, useRef, useEffect } from 'react';
-import { useRive } from 'rive-react';
+import React, {
+	useState,
+	setState,
+	useRef,
+	useEffect,
+	useContext,
+} from 'react';
 import Rive from 'rive-react';
-
 import { rephraseSentence } from '../services/RephraseRequestService';
+import { ModeContext } from '../ModeContext.js';
 
 function OutputBox(props) {
+	const { light, setMode } = useContext(ModeContext);
 	if (props.loading) {
 		return (
-		<div className="output-box">
-				<Rive className="loading-animation" src="Loading-Animation.riv" animations="Loading" />
+			<div className={light ? 'output-box' : 'dark-output-box'}>
+				<Rive
+					className="loading-animation"
+					src="Loading-Animation.riv"
+					animations="Loading"
+				/>
 				<div className="bottom-bar">
-					<button className="output-btn like-btn"><img className="icon like-icon" src={like_btn}/></button>
-					<button className="output-btn dislike-btn"><img className="icon dislike-icon" src={dislike_btn}/></button>
-					<button className="output-btn copy-btn"><img className="icon copy-icon" src={copy_icon} onClick={() => {navigator.clipboard.writeText(props.output)}}/></button>
+					<button
+						className={
+							light ? 'output-btn like-btn' : 'dark-output-btn like-btn'
+						}
+					>
+						<img className="icon like-icon" src={like_btn} />
+					</button>
+					<button
+						className={
+							light ? 'output-btn dislike-btn' : 'dark-output-btn dislike-btn'
+						}
+					>
+						<img className="icon dislike-icon" src={dislike_btn} />
+					</button>
+					<button
+						className={
+							light ? 'output-btn copy-btn' : 'dark-output-btn copy-btn'
+						}
+					>
+						<img
+							className={light ? 'icon copy-icon' : 'icon dark-copy-icon'}
+							src={copy_icon}
+							onClick={() => {
+								navigator.clipboard.writeText(props.output);
+							}}
+						/>
+					</button>
 				</div>
 			</div>
-	)
+		);
 	} else {
 		return (
-		<div className="output-box">
+			<div className={light ? 'output-box' : 'dark-output-box'}>
 				<div className="result">{props.output}</div>
 				<div className="bottom-bar">
-					<button className="output-btn like-btn"><img className="icon like-icon" src={like_btn}/></button>
-					<button className="output-btn dislike-btn"><img className="icon dislike-icon" src={dislike_btn}/></button>
-					<button className="output-btn copy-btn"><img className="icon copy-icon" src={copy_icon} onClick={() => {navigator.clipboard.writeText(props.output)}}/></button>
+					<button
+						className={
+							light ? 'output-btn like-btn' : 'dark-output-btn like-btn'
+						}
+					>
+						<img className="icon like-icon" src={like_btn} />
+					</button>
+					<button
+						className={
+							light ? 'output-btn dislike-btn' : 'dark-output-btn dislike-btn'
+						}
+					>
+						<img className="icon dislike-icon" src={dislike_btn} />
+					</button>
+					<button
+						className={
+							light ? 'output-btn copy-btn' : 'dark-output-btn copy-btn'
+						}
+					>
+						<img
+							className={light ? 'icon copy-icon' : 'icon dark-copy-icon'}
+							src={copy_icon}
+							onClick={() => {
+								navigator.clipboard.writeText(props.output);
+							}}
+						/>
+					</button>
 				</div>
 			</div>
-	)
+		);
 	}
 }
 export default function Rephrase() {
+	const { light, setMode } = useContext(ModeContext);
 	const [output, showResult] = useState('');
 	const [input, setInput] = useState('');
 	const inputRef = useRef(null);
@@ -45,8 +104,8 @@ export default function Rephrase() {
 		/*Listens to those changes and set changes as the value*/
 		setInput(event.target.value);
 		/* If the user removes their input, remove the previous output as well */
-		if (event.target.value == "") {
-			showResult("");
+		if (event.target.value == '') {
+			showResult('');
 		}
 	}
 	/*This function runs the moment you click on the arrow */
@@ -66,12 +125,11 @@ export default function Rephrase() {
 	}
 
 	useEffect(() => {
-		console.log(input);
-	}, [input]);
+		console.log('Rephrase: ', light);
+	}, [light]);
 
 	return (
-
-		<div className="IObox">
+		<div className={light ? 'IObox' : 'dark-IObox'}>
 			<form>
 				<textarea
 					ref={inputRef}
@@ -79,17 +137,19 @@ export default function Rephrase() {
 					type="text"
 					className="input"
 					placeholder="Type something..."
-
 					onChange={handleChange}
 					value={input}
 					align="left"
 				></textarea>
 			</form>
 			<button onClick={handleSubmit}>
-				<img src={arrow} className="arrow" alt="arrow"></img>
+				<img
+					src={arrow}
+					className={light ? 'arrow' : 'dark-arrow'}
+					alt="arrow"
+				></img>
 			</button>
 			<OutputBox output={output} loading={loading} />
 		</div>
-
 	);
 }
